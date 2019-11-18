e68f8d46550ec3f3f79e37e2b52f99592d2b7c78
Pervez alam <pervezalam777@gmail.com>
2019-11-18 21:25:02 +0530

    unit test cases added

--- src/dynamic-form/component/ui/input/Input.test.js
+++ src/dynamic-form/component/ui/input/Input.test.js
@@ -10,13 +10,35 @@ describe("Input component", () => {
     beforeEach(()=>{
         wrapper = shallow(<Input />);
     })
-    it("Should render input type text with provided value and placeholder", () => {
+    it("Should render input type text with provided values", () => {
         wrapper.setProps(intputTextObject)
         let input = wrapper.find('input').at(0);
         expect(input).toBeTruthy();
-        let props = wrapper.props();
-        //expect(props.value).toEqual('Pervez');
-        //expect(wrapper.placeholder).toBe('Your Name')
+        let props = input.props();
+        expect(props.value).toEqual('Pervez');
+        expect(props.placeholder).toEqual('Your Name');
+        expect(props["data-id"]).toEqual('text1')
+    })
+
+    it("should render text area with provided values", ()=>{
+        wrapper.setProps(inputTextAreaObject)
+        let input = wrapper.find('textarea').at(0);
+        expect(input).toBeTruthy();
+        let props = input.props();
+        expect(props.value).toEqual('Dummy comments');
+        expect(props.placeholder).toEqual('Comments');
+        expect(props["data-id"]).toEqual('ta1')
+    })
+
+    it("should render dropdown tag with provided values", ()=>{
+        wrapper.setProps(inputSelectObject)
+        let input = wrapper.find('select').at(0);
+        expect(input).toBeTruthy();
+        let props = input.props();
+        expect(props.value).toEqual('india');
+        expect(props.children[0].key).toEqual('india');
+        expect(props.children[1].key).toEqual('australia');
+        expect(props["data-id"]).toEqual('sel1')
     })
 })
 
@@ -28,4 +50,27 @@ const intputTextObject = {
         placeholder: 'Your Name'
     },
     value: 'Pervez',
+}
+
+const inputTextAreaObject = {
+    elementType: 'textarea',
+    elementId:"ta1",
+    elementConfig: {
+        type: 'text',
+        placeholder: 'Comments'
+    },
+    value: 'Dummy comments',
+}
+
+const inputSelectObject = {
+    elementType: 'select',
+    elementId: "sel1",
+    elementConfig: {
+        options: [
+            {value: 'india', displayValue: 'India'},
+            {value: 'australia', displayValue: 'Australia'}
+        ]
+    },
+    value: 'india'
+
 }
